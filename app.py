fba05f0d3f8436900bb742f9116110db457ff4cd
areebafarooq <areebafarooq1178@gmail.com>
2025-12-19 12:06:53 +0500

    updated

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, this is a test web app"
+    return "Hello, this is a test web app/n I am running this to test security tools"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
